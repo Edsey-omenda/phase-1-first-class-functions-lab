82c75416403b2f1502d54f40b9c43a7612708a5f
Edsey omenda <omendajames0317@gmail.com>
2022-06-09 00:33:18 +0300

    commit tree 0724bd7f2f95fbfb913e402871f8c8e1109bc8e0

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(){
+   
+      return ['Antonia','Nuru'];
+}
+
+const returnLastTwoDrivers = function(){
+   //const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+      return ['Amari','Mo'];
+}
+
+const selectingDrivers = (() => [returnFirstTwoDrivers, returnLastTwoDrivers])();
+/*const  selectingDrivers = (() => () => [
+      returnFirstTwoDrivers,
+      returnLastTwoDrivers,
+])();*/
+
+//var fare = 5;
+/*const createFareMultiplier = () =>{
+     return  fare * fare;}
+     */
+     const createFareMultiplier = function (multiplynumber){
+      return function (number){
+        return multiplynumber * number;
+      }
+}
+
+    const fareDoubler = function  (fare){
+          return (fare)*2;
+    }
+
+    const fareTripler = function  (fare){
+      return (fare)*3;
+}
+
+const selectDifferentDrivers = function (drivers, firstTwoDrivers)  {
+      return firstTwoDrivers(drivers);
+    }
+    /*const selectDifferentDrivers = function (drivers, lastTwoDrivers)  {
+      return lastTwoDrivers(drivers);
+    }*/
+
+
+
+    
